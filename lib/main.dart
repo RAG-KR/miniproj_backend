e8cd15157701996cdeeb6d49390fbbfb863f53a6
PARDY1166 <puli.cs21@bmsce.ac.in>
2024-02-04 15:45:50 +0530

    2nd Commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,17 @@ import 'package:t_store/utils/theme/theme.dart';
 
 import 'features/shop/screens/onboarding/onboarding.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+// ...
+
+
+
+Future<void> main() async {
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
   runApp(const App());
 }
 
